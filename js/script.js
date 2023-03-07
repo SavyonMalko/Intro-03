872c29c09e2eabee3d876da6012fd4c7ddfb9aac
SavyonM <124379798+SavyonMalko@users.noreply.github.com>
2023-03-07 22:05:39 +0000

    finished added button yktv

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Ms Raffin All rights reserved
+//
+// Created by: Ms Raffin
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays "Hello, World!"
+  
+  document.getElementById("hello-world").innerHTML = "Hello, I love JavaScript!"
+}
